ef34690b3e42afba1e92db417326cbd81c95410d
chand1997 <chanduchallahalli@gmail.com>
2025-01-16 08:47:45 +0530

    Adding 2 numbers

--- script-04.sh
+++ script-04.sh
@@ -3,9 +3,9 @@
 NUMBER1=$1
 NUMBER2=$2
 
-TIMESTAMP=$(($date))
+TIMESTAMP=$($date)
 
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "Timestamp : $TIMESTAMP"
+echo "Script executed at : $TIMESTAMP"
 echo "The sum is of ${NUMBER1} and ${NUMBER2} : $SUM"
