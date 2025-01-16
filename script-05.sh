15f5a20075aa25cd09d1134d46da9640f1eaee37
chand1997 <chanduchallahalli@gmail.com>
2025-01-16 09:02:07 +0530

    using Arrays

--- script-05.sh
+++ script-05.sh
@@ -6,3 +6,10 @@ MOVIES=("kgf" "kantara" "tagaru")
 
 
 echo "First movie in array is : ${MOVIES[0]}"
+echo "Second movie in array is : ${MOVIES[1]}"
+echo "Third movie in array is : ${MOVIES[2]}"
+
+#how to access all movies at once
+
+echo "Movies : ${MOVIES[@]}"
+
