dc6d7fad9a95a936ba9cb6d50ae91df899193746
chand1997 <chanduchallahalli@gmail.com>
2025-01-16 10:14:12 +0530

    if using single/double square brackets

--- script-07.sh
+++ script-07.sh
@@ -2,8 +2,8 @@
 
 #Program to write whether day is weekday or weekend
 
-#if [[ $(date +%w) == 0 || $(date +%w) == 6 ]]   
-if [ $(date +%w) -eq 0 ] || [$(date +%w) -eq 6 ]
+if [[ $(date +%w) == 0 || $(date +%w) == 6 ]]   
+#if [ $(date +%w) -eq 0 ] || [$(date +%w) -eq 6 ]
 then
 echo "Weekend"
 else 
