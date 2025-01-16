f9ed0d9b7a37b6b157168fa6db76227340c63f73
chand1997 <chanduchallahalli@gmail.com>
2025-01-16 16:38:20 +0530

    added some exit codes

--- script-09.sh
+++ script-09.sh
@@ -5,20 +5,57 @@
 if [ $(id -u) -ne 0 ]
 then
 echo "should login as root user"
-exit 1
+exit 1 
+ #  "exit 0" means the further lines will execute  otherwise the code stops there itself.
+ #  we can write "exit n" to stop executing the further code(n--> any number other than 0)
 fi
 
 echo "Installing mysql-server"
 dnf install mysql-server -y
 
+# $? gives exit code of previous code
+
+if [ $? -ne 0 ]
+then
+echo "Installation Failed"
+exit 1
+else
+echo "Installation successful"
+fi
+
 echo "Starting mysql server"
 systemctl start mysqld
+
+if [ $? -ne 0 ]
+then
+echo "Failed to start mysql"
+exit 1
+else
+echo "Mysql started successful"
+fi
 
 echo "Enabling mysql server"
 systemctl enable mysqld
 
-echo "Checking status of mysql server"
-systemctl status mysqld
+if [ $? -ne 0 ]
+then
+echo "Failed to enable mysql"
+exit 1
+else
+echo "Mysql enabled successful"
+fi
 
-echo "Installation successful"
+echo "Checking status of mysql server"
+systemctl status mysqld | grep -q "active (running)"
+# the above code checks for pattern in double quotes and if it finds it will retun exit 0.
+
+if [ $? -ne 0 ]
+then
+echo "Mysql is not running."
+exit 1
+else
+echo "Mysql is running successful"
+fi
+
+
 
