56dcb0bb14da6aa83700675bb7d9791f2a3a194f
chand1997 <chanduchallahalli@gmail.com>
2025-01-16 09:47:51 +0530

    Program to find weekday or weekend

--- script-07.sh
+++ script-07.sh
@@ -2,7 +2,8 @@
 
 #Program to write whether day is weekday or weekend
 
-if [[$(date +%w) == 0 || $(date +%w) == 6 ]] then
+if [[$(date +%w) == 0 || $(date +%w) == 6 ]]
+then
 echo "Weekend"
 else 
 echo "Weekday"
