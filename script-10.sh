4fcbaccda8d0222cd2e16e56cdbdd11be444be7f
chand1997 <chanduchallahalli@gmail.com>
2025-01-16 17:46:48 +0530

    writing functions

--- script-10.sh
+++ script-10.sh
@@ -3,12 +3,6 @@
 #Improvised way of writing script-09
 
 VALIDATE(){
-    if [[ $2 -eq 0 && $? -eq 0 ]]
-    then
-    echo "$1"
-    exit 1
-    fi
-
     if [ $? -ne 0 ]
     then
     echo "$1"
@@ -28,7 +22,7 @@ fi
 
 dnf list installed | grep -q "mysql-server"
 
-VALIDATE Mysql_server_already_installed
+VALIDATE Mysql_server_already_installed Mysql_server_not_installed
 
 dnf install mysql-server -y
 
@@ -44,7 +38,7 @@ systemctl enable mysqld
 VALIDATE Mysql_server_failed_to_enable Mysql_server_successfully_enabled
 
 
-sytemctl status mysqld | grep -q "active (running)"
+systemctl status mysqld | grep -q "active (running)"
 
 VALIDATE Mysql_server_not_active Mysql_server_successfully_running
 
