2358315757214e24bad49f20213b4ef14b9d1c7d
chand1997 <chanduchallahalli@gmail.com>
2025-01-16 08:10:07 +0530

    how to take confidential info

---
+++ script-03.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# How to take variables like username and password(confidentials)
+
+echo "Enter username :"
+
+read -s USERNAME   # -s -> to hide whatever entered
+
+echo "USERNAME ENTERED : $USERNAME"  # it is showing just because we printed it.
+
+echo "Enter Password :"
+
+read -s PASSWORD
+
+echo "Password entered"
