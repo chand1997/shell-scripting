703d1a1f51444e2952e0e5ff9ce3212012af5ed2
chand1997 <chanduchallahalli@gmail.com>
2025-01-16 16:01:42 +0530

    script to install mysql

--- script-09.sh
+++ script-09.sh
@@ -2,8 +2,19 @@
 
 #Installing mysql
 
-sudo su -
+if[ $(id -u) -ne 0 ]
+then
+echo "should login as root user"
+exit 1
+fi
+echo "Installing mysql-server"
 dnf install mysql-server -y
+
+echo "Starting mysql server"
 systemctl start mysqld
+
+echo "Enabling mysql server"
 systemctl enable mysqld
+
+echo "Checking status of mysql server"
 systemctl status mysqld
