f3276aeb35dc552e3744abee1006a850d14080e7
chand1997 <chanduchallahalli@gmail.com>
2025-01-17 09:17:18 +0530

    adding logs to store large outputs

---
+++ script-12.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#In this we are learning how to write log files.
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/shellscript_logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGFILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIME_STAMP.log"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2  $R FAILED $N "
+        exit 1
+    else
+        echo -e "$2 $G SUCCESS $N "
+    fi
+
+}
+echo "Script started executing at $TIME_STAMP" &>>$LOGFILE_NAME
+if [ $(id -u) -ne 0 ]; then
+    echo "Give root access"
+    exit 1
+fi
+
+echo "Checking whether mysql-server is already installed"
+
+dnf list installed mysql-server | grep -q "mysql-server"
+
+if [ $? -eq 0 ]; then
+    echo -e " $Y Mysql-server already INSTALLED $N "
+    exit 1
+fi
+
+echo "Mysql-server installing"
+
+dnf install mysql-server -y &>>$LOGFILE_NAME
+
+VALIDATE $? Mysql_Installation
+
+echo "Starting mysql-server"
+
+systemctl start mysqld &>>$LOGFILE_NAME
+
+VALIDATE $? Starting_Mysql
+
+echo "Enabling Mysql-server"
+
+systemctl enable mysqld &>>$LOGFILE_NAME
+
+VALIDATE $? Enabling_Mysql
+
+echo "Checking status of mysql-server"
+
+systemctl status mysqld | grep -q "active (running)"
+
+VALIDATE $? Status_checking
+
+echo "Mysql-server successfully running"
+
+sleep 2
+
+echo "Deleting mysql-server"
+
+dnf remove mysql-server -y &>>$LOGFILE_NAME
+
+VALIDATE $? Removing_Mysql_server
