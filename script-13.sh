a6f9ab1ddac1408500d7fc4246b3dd875458cb5b
chand1997 <chanduchallahalli@gmail.com>
2025-01-17 09:47:40 +0530

    loops script13

--- script-13.sh
+++ script-13.sh
@@ -4,12 +4,12 @@
 
 #Print 1-N
 echo "Printing 1-10"
-for i in {1..10}; do
-    $i
+for i in $(seq 1 10); do
+    echo $i
 done
 
 echo "Enter Number : "
 read NUMBER
 for i in $(seq 1 $NUMBER); do
-    $i
+    echo $i
 done
